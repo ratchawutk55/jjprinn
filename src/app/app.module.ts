cd755c8795b2224831ac0803d6b461ab1de73a50
Ratchawut Keunmamuang <ratchawut.k@connex.local>
2018-11-23 16:34:57 +0700

    add car list , car detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MockService } from './service/mock.service';
+import { CarListComponent } from './components/car-list/car-list.component';
+import { CarDetailComponent } from './components/car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    CarDetailComponent
   ],
   imports: [
     BrowserModule,
